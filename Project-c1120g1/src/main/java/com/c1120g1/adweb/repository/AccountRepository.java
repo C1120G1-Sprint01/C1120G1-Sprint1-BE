2a17d44d390ab844f7d471228d9a2872e2566f99
Hoangtq1710 <hoangtq1710@gmail.com>
2021-05-28 02:07:16 +0700

    Hoangtq update

--- Project-c1120g1/src/main/java/com/c1120g1/adweb/repository/AccountRepository.java
+++ Project-c1120g1/src/main/java/com/c1120g1/adweb/repository/AccountRepository.java
@@ -4,4 +4,6 @@ import com.c1120g1.adweb.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    Account findByUsername(String username);
 }
--- Project-c1120g1/src/main/java/com/c1120g1/adweb/service/AccountService.java
+++ Project-c1120g1/src/main/java/com/c1120g1/adweb/service/AccountService.java
@@ -1,4 +1,8 @@
 package com.c1120g1.adweb.service;
 
+import com.c1120g1.adweb.entity.Account;
+
 public interface AccountService {
+
+    Account findByUsername(String username);
 }
