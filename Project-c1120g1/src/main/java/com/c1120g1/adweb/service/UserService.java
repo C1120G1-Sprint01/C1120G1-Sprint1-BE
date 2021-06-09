c843d0efae2768e483d11a78d4a7355a6f2fca1a
Hoangtq1710 <hoangtq1710@gmail.com>
2021-06-10 01:31:32 +0700

    Hoangtq update

--- Project-c1120g1/src/main/java/com/c1120g1/adweb/service/UserService.java
+++ Project-c1120g1/src/main/java/com/c1120g1/adweb/service/UserService.java
@@ -45,6 +45,6 @@ public interface UserService {
      * */
     List<UserStatisticsDTO> statisticUser(String startDate, String endDate);
 
-    void saveUserGoogle(User user);
+    void saveUserSocial(User user);
 
 }
